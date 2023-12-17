238ca6fd0d4e34c1d45f62ce9bc47b19e6602425
khetumewada <khetu.mewada@gmail.com>
2023-12-17 17:55:18 +0530

    all update

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path("", views.index, name="blogHome"),
+    path("blogpost/<int:id>", views.blogpost, name="blogHome")
+    # path("blogpost/", views.blogpost, name="blogHome")
+]
